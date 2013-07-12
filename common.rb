00dd87cb9dfdeca890588dbc14f9fadf726a6c2d
Travis Roberts <traviskroberts@gmail.com>
2013-07-12 16:44:03 -0500

    Changed rvm setup to use the new .ruby-version, .ruby-gemset format instead of .rvmrc. Defaulting new applications to use Ruby 2.0.0.

--- common.rb
+++ common.rb
@@ -4,15 +4,15 @@ require File.join(File.dirname(__FILE__), 'core_extensions.rb')
 require 'rvm'
 run "rvm gemset create #{app_name}"
 RVM.gemset_use! app_name # `run "rvm gemset use #{app_name}"` doesn't work -- rvm still uses the terminal's current gemset
-create_file '.rvmrc', "rvm gemset use #{app_name}"
-run 'rvm rvmrc trust'
+create_file '.ruby-version', '2.0.0'
+create_file '.ruby-gemset', "#{app_name}"
 
 initialize_template
 
 load_options
 
 #loop through the recipes and apply each one
-required_recipes = %w(database gitignore gemfile bourbon_neat chosen production bundle routes flash_messages application schedule rspec whiskey_disk foreman active_admin authentication custom_error_pages guard spork_rspec git heroku readme)
+required_recipes = %w(database gitignore gemfile bourbon_neat chosen production bundle routes flash_messages application schedule rspec whiskey_disk foreman active_admin authentication custom_error_pages guard spork_rspec git heroku)
 required_recipes.each {|required_recipe| apply recipe(required_recipe)}
 
 # semi-hack to make sure our post install messages are output last

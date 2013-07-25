f3d382e368d99067b2df2db678f3af84746e24e5
Cade Truitt <ctruitt@centresource.com>
2013-07-25 11:05:33 -0500

    clean up routes file

--- templates/routes/config/routes.rb
+++ templates/routes/config/routes.rb
@@ -1,11 +1,11 @@
 
   # authentication routes
-  match '/login',                 :to => 'user_session#new',                      :as => :login,           :via => :get
-  match '/login',                 :to => 'user_session#create',                   :as => :login,           :via => :post
-  match '/logout',                :to => 'user_session#destroy',                  :as => :logout,          :via => :delete
-  match '/forgot_password',       :to => 'user_session#forgot_password',          :as => :forgot_password, :via => :get
-  match '/forgot_password',       :to => 'user_session#send_reset_password_link', :as => :forgot_password, :via => :post
-  match '/reset_password/:token', :to => 'user_session#acquire_password',         :as => :reset_password,  :via => :get
-  match '/reset_password/:token', :to => 'user_session#reset_password',           :as => :reset_password,  :via => :post
-  match '/register',              :to => 'users#new',                             :as => :register,        :via => :get
-  match '/register',              :to => 'users#create',                          :as => :register,        :via => :post
+  get :login,                     :to => 'user_session#new',                      :as => :login
+  post :login,                    :to => 'user_session#create',                   :as => :login
+  delete :logout,                 :to => 'user_session#destroy',                  :as => :logout
+  get :forgot_password,           :to => 'user_session#forgot_password',          :as => :forgot_password
+  post :forgot_password,          :to => 'user_session#send_reset_password_link', :as => :forgot_password
+  get '/reset_password/:token',   :to => 'user_session#acquire_password',         :as => :reset_password
+  post '/reset_password/:token',  :to => 'user_session#reset_password',           :as => :reset_password
+  get :register,                  :to => 'users#new',                             :as => :register
+  post :register,                 :to => 'users#create',                          :as => :register

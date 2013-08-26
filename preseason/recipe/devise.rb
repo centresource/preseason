5000d2350a526d8bbf69ce1a78768fa1589afcce
Cade Truitt <ctruitt@centresource.com>
2013-08-26 14:52:00 -0500

    run devise installer even with activeadmin since AA falls flat

--- preseason/recipe/devise.rb
+++ preseason/recipe/devise.rb
@@ -2,7 +2,7 @@ class Preseason::Recipe::Devise < Preseason::Recipe
   def prepare
     return unless config.authentication.devise?
     
-    generate 'devise:install' unless config.authentication.active_admin?
+    generate 'devise:install'
     generate 'devise', 'user'
     insert_into_file 'config/environments/development.rb', "\n  config.action_mailer.default_url_options = { :host => 'localhost:3000' }\n", :before => /^end$/
   end

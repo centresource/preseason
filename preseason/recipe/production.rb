d66b697f60649de553a10b7506e1a72ad29afc25
Cade Truitt <ctruitt@centresource.com>
2013-10-31 09:16:45 -0500

    Add default assets to precompile array fixes #22

--- preseason/recipe/production.rb
+++ preseason/recipe/production.rb
@@ -1,15 +1,36 @@
 class Preseason::Recipe::Production < Preseason::Recipe
+  PRODUCTION = 'config/environments/production.rb'
+
   def prepare
     enable_lograge unless config.heroku.use?
     configure_heroku_rails_deflate
+    add_precompile_assets
   end
-  
+
   private
   def enable_lograge
-    insert_into_file 'config/environments/production.rb', "\n  config.lograge.enabled = true\n", :before => /^end$/
+    insert_into_file PRODUCTION, "\n  config.lograge.enabled = true\n", :before => /^end$/
   end
-  
+
   def configure_heroku_rails_deflate
-    gsub_file 'config/environments/production.rb', 'config.serve_static_assets = false', 'config.serve_static_assets = true'
+    gsub_file PRODUCTION, 'config.serve_static_assets = false', 'config.serve_static_assets = true'
+  end
+
+  def add_precompile_assets
+    insert_into_file PRODUCTION, precompile_array, :before => /^end$/
+  end
+
+  def precompile_array
+    str = <<-TXT
+
+  config.assets.precompile += %w(
+    screen.css
+    ie8.js
+    #{'active_admin.js' if config.authentication.active_admin?}
+    #{'active_admin.css' if config.authentication.active_admin?}
+  )
+    TXT
+
+    str.gsub /^\s{4}\n/, ''
   end
 end

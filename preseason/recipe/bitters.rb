75725705673a8296ba3aa541d46a6f12b723f8c7
Desmond Cain <desmondcain@gmail.com>
2013-12-30 14:16:45 -0600

    add inject_into_file action

--- preseason/recipe/bitters.rb
+++ preseason/recipe/bitters.rb
@@ -1,7 +1,8 @@
 class Preseason::Recipe::Bitters < Preseason::Recipe
 	def prepare
-		inside 'app/assets/stylesheets/' do
-    	run "bitters install" #, :capture => true, :verbose => false
+		inside 'app/assets/stylesheets' do
+    	run 'bitters install'
+    	inject_into_file 'screen.scss', "@import \"bitters/bitters\";\n", :after => "@import \"bourbon\";\n"
     end
 	end
 end

175ac2d600dd488926dd582dbc68b39b259b9bef
Travis Roberts <traviskroberts@gmail.com>
2014-01-31 14:13:57 -0600

    Rename scss assets to be css.scss.

--- preseason/recipe/bitters.rb
+++ preseason/recipe/bitters.rb
@@ -2,6 +2,7 @@ class Preseason::Recipe::Bitters < Preseason::Recipe
   def prepare
     install_bitters
     cleanup_bitters
+    rename_assets
   end
 
   private
@@ -9,8 +10,8 @@ class Preseason::Recipe::Bitters < Preseason::Recipe
   def install_bitters
     inside 'app/assets/stylesheets' do
       run 'bitters install'
-      `cp -R bitters/* base/`
-      `cat base/mixins/imports.scss >> base/mixins/_base.scss`
+      run 'cp -R bitters/* base/'
+      run 'cat base/mixins/imports.scss >> base/mixins/_base.scss'
     end
   end
 
@@ -18,5 +19,16 @@ class Preseason::Recipe::Bitters < Preseason::Recipe
     remove_dir 'app/assets/stylesheets/bitters'
     remove_file 'app/assets/stylesheets/base/_bitters.scss'
     remove_file 'app/assets/stylesheets/base/mixins/imports.scss'
+    create_file 'app/assets/stylesheets/modules/.gitkeep'
+    create_file 'app/assets/stylesheets/pages/.gitkeep'
+  end
+
+  def rename_assets
+    Dir.glob("app/assets/stylesheets/**/*.scss").each do |path|
+      unless path.end_with?('.css.scss')
+        new_path = path.gsub('.scss', '.css.scss')
+        run("mv #{path} #{new_path}")
+      end
+    end
   end
 end

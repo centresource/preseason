d95ed69a8fbbbc8075838b5dffafe6f98af975b3
Max Beizer <max.beizer@gmail.com>
2014-07-18 17:31:16 -0500

    Update errors routes to include via hash

--- lib/preseason/templates/custom_error_pages/config/routes.rb
+++ lib/preseason/templates/custom_error_pages/config/routes.rb
@@ -1,5 +1,5 @@
 
   # custom error routes
-  match '/404' => 'errors#not_found'
-  match '/422' => 'errors#unprocessable_entity'
-  match '/500' => 'errors#application_error'
+  match '/404' => 'errors#not_found'            , :via => [:get, :post]
+  match '/422' => 'errors#unprocessable_entity' , :via => [:get, :post]
+  match '/500' => 'errors#application_error'    , :via => [:get, :post]

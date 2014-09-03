4e158ca8dc319d75d805bcc7bca7d0f2e0625f8b
Rian Rainey <hello@rian.me>
2014-09-03 11:37:02 -0500

    Added ie8.js to precompile list.

--- lib/preseason/recipe/initializer.rb
+++ lib/preseason/recipe/initializer.rb
@@ -1,12 +1,20 @@
 class Preseason::Recipe::Initializer < Preseason::Recipe
+  attr_accessor :assets
+
   def prepare
+    precompile_assets
     initialize_assets
   end
 
   private
+  def precompile_assets
+    @assets = %w(screen.css)
+    @assets << 'ie8.js' if config.ie8.enabled?
+  end
+
   def initialize_assets
     old_line = '# Rails.application.config.assets.precompile += %w( search.js )'
-    new_line = 'Rails.application.config.assets.precompile += %w( screen.css )'
+    new_line = "Rails.application.config.assets.precompile += #{@assets}"
     gsub_file 'config/initializers/assets.rb', old_line, new_line
   end
 end

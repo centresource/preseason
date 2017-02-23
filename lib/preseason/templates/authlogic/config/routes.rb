f4cd621e5caf81c914825c609cac4ffe07bf2787
Emma Beaton <emma.beaton@gmail.com>
2017-02-23 12:47:24 -0600

    Fix routing bugs and dir name to resolve Authlogic errors.

--- lib/preseason/templates/authlogic/config/routes.rb
+++ lib/preseason/templates/authlogic/config/routes.rb
@@ -1,11 +1,12 @@
 
   # authentication routes
-  get :login,                     to: 'user_sessions#new'
-  post :login,                    to: 'user_sessions#create'
-  delete :logout,                 to: 'user_sessions#destroy'
+  resource :user_sessions
+  resources :users
+
+  get '/signup', to: 'users#new'
+  get '/login', to: 'user_sessions#new'
+  delete '/logout', to: 'user_sessions#destroy'
   get :forgot_password,           to: 'user_sessions#forgot_password'
   post :forgot_password,          to: 'user_sessions#send_reset_password_link'
   get '/reset_password/:token',   to: 'user_sessions#acquire_password'
   post '/reset_password/:token',  to: 'user_sessions#reset_password'
-  get :register,                  to: 'users#new'
-  post :register,                 to: 'users#create'

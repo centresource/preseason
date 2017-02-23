0a8be7fb405416dabc8650587fa4adf8521d42df
Emma Beaton <emma.beaton@gmail.com>
2017-02-23 16:43:53 -0600

    Remove Spork recipe, add Zeus recipe.

--- lib/preseason/recipe/spork_rspec.rb
+++
@@ -1,7 +0,0 @@
-class Preseason::Recipe::SporkRspec < Preseason::Recipe
-  def prepare
-    remove_file 'spec/spec_helper.rb'
-    create_file 'spec/spec_helper.rb', parse_template('spec/spec_helper.erb')
-    remove_file 'test'
-  end
-end
